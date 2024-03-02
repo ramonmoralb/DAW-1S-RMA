e492ab70e7c1889f5a173bf5231b00de94c34a60
ramonmoreno1 <ramonmoreno1@hotmail.es>
2024-03-02 08:17:01 +0100

    Finalizado ejerecicio 3 base de datos

--- BaseDeDatos/UD5Consultas/EjercicioConsultas3RMA.sql
+++ BaseDeDatos/UD5Consultas/EjercicioConsultas3RMA.sql
@@ -1,7 +1,6 @@
 -- 1. Número y nombre de los vendedores que oferten alguna de las piezas que pueden ser suministradas por el vendedor número 1,
 -- pero que no oferten ninguna de las que puedan ser suministradas por el vendedor número 2.
-SELECT *FROM VENDEDOR;
-SELECT *FROM PRECIOSUM;
+
 
 SELECT V.NUMVEND, V.NOMVEND FROM VENDEDOR V 
 JOIN PRECIOSUM PS ON PS.NUMVEND = V.NUMVEND
@@ -12,9 +11,6 @@ JOIN VENDEDOR V ON V.NUMVEND=PS.NUMVEND WHERE PS.NUMVEND=2)
 GROUP BY  V.NUMVEND, V.NOMVEND    
 ;
 
-
-SELECT PS.NUMPIEZA, PS.NUMVEND FROM PRECIOSUM PS 
-JOIN VENDEDOR V ON V.NUMVEND=PS.NUMVEND WHERE PS.NUMVEND=2; 
 
 -- 2. Obtener el número y el nombre de los vendedores y la cantidad de piezas que pueden suministrar a un precio entre 15 y 20 euros, ordenado por el nombre de vendedor.
 
@@ -31,9 +27,7 @@ ORDER BY V.NOMVEND
 
 -- 3. Obtener, para el vendedor que cumple que la diferencia de precio al que le compramos una pieza y el precio que nos había ofrecido por ella sea máxima, el número de vendedor, su nombre
 -- y la diferencia media entre el precio al que nos vende las piezas y el que nos había ofrecido por las mismas.
-SELECT * FROM PRECIOSUM;
-SELECT * FROM LINPED;
-SELECT * FROM PIEZA;
+
 
 SELECT V.NUMVEND, V.NOMVEND, AVG(L.PRECIOCOMPRA-P.PRECIOVENT) FROM VENDEDOR V
 JOIN PRECIOSUM PS ON PS.NUMVEND=V.NUMVEND
@@ -41,10 +35,9 @@ JOIN PIEZA P ON P.NUMPIEZA=PS.NUMPIEZA
 JOIN LINPED L ON L.NUMPIEZA=P.NUMPIEZA    
 WHERE (L.PRECIOCOMPRA-P.PRECIOVENT)=(SELECT MAX(L.PRECIOCOMPRA-P.PRECIOVENT) FROM LINPED L
 JOIN PIEZA P ON P.NUMPIEZA=L.NUMPIEZA)
-    GROUP BY  V.NUMVEND, V.NOMVEND
+GROUP BY  V.NUMVEND, V.NOMVEND
 ;
-SELECT MAX(L.PRECIOCOMPRA-P.PRECIOVENT) FROM LINPED L
-JOIN PIEZA P ON P.NUMPIEZA=L.NUMPIEZA;
+
 
 
 
@@ -53,40 +46,15 @@ JOIN PIEZA P ON P.NUMPIEZA=L.NUMPIEZA;
 SELECT P.NOMPIEZA FROM PIEZA P       
 JOIN LINPED L ON L.NUMPIEZA=P.NUMPIEZA
 WHERE P.NOMPIEZA LIKE '%TECLADO%' AND
-L.NUMPEDIDO IN (SELECT PE.NUMPEDIDO FROM PEDIDO PE 
+L.NUMPIEZA IN (SELECT L.NUMPIEZA FROM PEDIDO PE 
 JOIN LINPED L ON L.NUMPEDIDO=PE.NUMPEDIDO  
-WHERE  EXTRACT(MONTH FROM fecha) = 5
-GROUP BY PE.NUMPEDIDO)
+WHERE  EXTRACT(MONTH FROM FECHARECEP) = 5
+) GROUP BY P.NOMPIEZA 
 ;
-
-/*EXPLICACIÓN PARA ENTENDER MEJOR
-seleccioname el nombre de la pieza de la tabla pieza
-que en la tabla linped y en la tabla pieza tengan el mismo numero de pieza
-donde el nombre de la pieza contenga %TECLADO%
-y el numero de pedido aparezca en la subconsulta que
-devuelva los pedidos en fecha de mayo de cualquier año
-agrupados por numero de pedido para que funcione
-*/
-/*-- COMPROBACIONES Y PRUEBAS
-SELECT * FROM LINPED;
-SELECT * FROM PEDIDO;
-SELECT * FROM PIEZA;
-
-SELECT PE.NUMPEDIDO FROM PEDIDO PE 
-JOIN LINPED L ON L.NUMPEDIDO=PE.NUMPEDIDO  
-WHERE  EXTRACT(MONTH FROM fecha) = 5;
-
-SELECT P.NOMPIEZA, P.NUMPIEZA, L.NUMPEDIDO, PE.FECHA FROM PIEZA P 
-JOIN LINPED L ON P.NUMPIEZA=L.NUMPIEZA
-JOIN PEDIDO PE ON PE.NUMPEDIDO=L.NUMPEDIDO;
-*/
 
 
 -- 5. Obtener un listado en el que figure el número y nombre de la pieza , junto con el número y nombre de vendedor que nos ha ofertado la pieza, pero al que nunca se la hemos solicitado.
 
---REVISAR
-SELECT * FROM VENDEDOR;
-SELECT * FROM PRECIOSUM;
 
 SELECT PS.NUMPIEZA, P.NOMPIEZA, V.NUMVEND, V.NOMVEND FROM VENDEDOR V
 JOIN PRECIOSUM PS ON PS.NUMVEND=V.NUMVEND
