c584c805a39fe1a2ed3c5fc2ed20b49d192d2e1b
ramonmoreno1 <ramonmoreno1@hotmail.es>
2024-03-27 19:29:45 +0100

    empezado de 0, funciona como pide el enunciado, pendiente de dar formato,

---
+++ LenguajeDeMarcas/RamonMorenoAlbert_practica1PHP/Persona.php
@@ -0,0 +1,63 @@
+<!-- Practica PHP Ramón Moreno Albet -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        $mostrar = false;
+        if (isset($_POST['enviar'])) {
+            $_SESSION['dni'] = $_POST['dni'];
+            $_SESSION['idioma'] = $_POST['idioma'];
+            $_SESSION['publico'] = $_POST['publico'];
+            $_SESSION['zona'] = $_POST['zona'];
+            $mostrar = true;
+        }
+        if (isset($_POST['borrar'])) {
+            $_POST = []; // Limpiar $_POST solo si se ha enviado el formulario "borrar"
+        }
+        ?>
+        <div>
+            <h2>Preferencias</h2>
+            <?php
+            if ($mostrar) {
+                echo '<p>La info ha sido guardada</p>';
+                $mostrar = false;
+            }
+            ?>
+            <form method="POST" action="Persona.php">
+                <label for="dni">DNI: </label>
+                <input type="text" name="dni">
+                <label for="idioma">Idioma: </label>
+                <select id="id" name="idioma">
+                    <option value="ESPAÑOL" <?php if (isset($_SESSION['idioma']) && $_SESSION['idioma'] == 'ESPAÑOL') echo 'selected="selected"'; ?>>ESPAÑOL</option>
+                    <option value="INGLES" <?php if (isset($_SESSION['idioma']) && $_SESSION['idioma'] == 'INGLES') echo 'selected="selected"'; ?>>INGLÉS</option>
+                    <option value="VALENCIANO" <?php if (isset($_SESSION['idioma']) && $_SESSION['idioma'] == 'VALENCIANO') echo 'selected="selected"'; ?>>VALENCIANO</option>
+                </select>
+                <label for="publico">PERFIL PÚBLICO: </label>
+                <select id="" name="publico"> 
+                    <option value="SI" <?php if (isset($_SESSION['publico']) && $_SESSION['publico'] == 'SI') echo 'selected="selected"'; ?> >SI</option>
+                    <option value="NO" <?php if (isset($_SESSION['publico']) && $_SESSION['publico'] == 'NO') echo 'selected="selected"'; ?>>NO</option>
+                </select>
+                <label for="zona">Zona: </label>
+                <select id="" name="zona">
+                    <option value="ZONA+1" <?php if (isset($_SESSION['zona']) && $_SESSION['zona'] == 'ZONA+1') echo 'selected="selected"'; ?>>ZONA+1</option>
+                    <option value="ZONA0" <?php if (isset($_SESSION['zona']) && $_SESSION['zona'] == 'ZONA0') echo 'selected="selected"'; ?> >ZONA0</option>
+                    <option value="ZONA-1" <?php if (isset($_SESSION['zona']) && $_SESSION['zona'] == 'ZONA-1') echo 'selected="selected"'; ?>>ZONA-1</option>
+                </select>
+
+
+
+                <input type="submit" name="enviar" value="enviar">
+            </form>
+        </div>
+        <form method="POST" action="Persona.php">
+            <input type="submit" name="borrar" value="Borrar">
+        </form>
+        <a href="listar.php">Mostrar preferencias</a>
+
+    </body>
+</html>
--- LenguajeDeMarcas/RamonMorenoAlbert_practica1PHP/listar.php
+++ LenguajeDeMarcas/RamonMorenoAlbert_practica1PHP/listar.php
@@ -1,22 +1,57 @@
-<?php
-session_start(); //sin sesion_start(); las variables se pierden
-
-
-//haciendo pruebas de como recibir las variables
-$idioma = $_SESSION['idioma'];
-$publico = $_SESSION['selectperfilpublico'];
-$zona = $_SESSION['zona'];
-?>
+<!-- Practica PHP Ramón Moreno Albet -->
 <!DOCTYPE html>
+<?php
+session_start();
+$boton_borrar = true;
+
+if (isset($_POST['borrar'])) {
+    $_POST = [];
+    $_SESSION = [];
+    $boton_borrar = false; // Desactiva el botón después de borrar las preferencias
+}
+
+if (!empty($_SESSION)) {
+    $dni = $_SESSION['dni'];
+    $idioma = $_SESSION['idioma'];
+    $publico = $_SESSION['publico'];
+    $zona = $_SESSION['zona'];
+} else {
+    $dni = "";
+    $idioma = "";
+    $publico = "";
+    $zona = "";
+    $boton_borrar = false; // No hay preferencias, por lo tanto, desactiva el botón
+}
+?>
+
 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>LListar</title>
+        <style>
+            #conte{
+                border: solid 1px black;
+                width: 450px;
+                padding: 10px;
+            }
+        </style>
     </head>
     <body>
-        <?php
-        echo $idioma;
-?>
+        <div id="conte">
+            <p>DNI: <?php echo $dni ?></p>
+            <p>IDIOMA: <?php echo $idioma ?></p>
+            <p>PERFIL PUBLICO: <?php echo $publico ?></p>
+            <p>ZONA HORARIA: <?php echo $zona ?></p>
+
+            <?php if ($boton_borrar) { ?>
+                <form action="listar.php" method="POST">
+                    <input type="submit" name="borrar" value="Borra preferencias">
+                <?php } ?>
+                <?php if (!$boton_borrar) { ?>
+                    <a href="persona.php">Volver</a>
+                <?php } ?>
+            </form>
+        </div>
     </body>
 </html>
