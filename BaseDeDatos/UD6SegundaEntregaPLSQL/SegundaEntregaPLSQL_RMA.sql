cc06c523e53727c13e2fc5bf54abfa9223c7af7e
ramonmoreno1 <ramonmoreno1@hotmail.es>
2024-03-31 18:01:02 +0200

    Practica pendiente de pruebas

--- BaseDeDatos/UD6SegundaEntregaPLSQL/SegundaEntregaPLSQL_RMA.sql
+++ BaseDeDatos/UD6SegundaEntregaPLSQL/SegundaEntregaPLSQL_RMA.sql
@@ -2,45 +2,51 @@
 --Para cálcular esta contraseña debes crear una función o procedimiento adicional que reciba el numero de caracteres que debe tener la contraseña y la genere de forma automática.
 --Esta contraseña debe contener letras mayúsculas, minúsculas, números y signos de puntuación.
 --Puedes crear cualquier tabla adicional que consideres necesaria, además de las funciones proporcionadas por el SGBD.
---Ten en cuenta la gestión de errores cuando realices la implementación de tu trigger
+--Ten en cuenta la gestión de errores cuando realices la implementación de tu trigger.
 
---creo la función que genera contraseña
-create or replace function generar_contrasenya (v_num_caracteres number) return varchar2
-    is
+--Creación de procedimiento que genera la cotraseña.
+CREATE OR REPLACE FUNCTION generar_contrasenya (v_num_caracteres number) RETURN varchar2
+    IS
      v_contrasenya varchar2(100);
      v_caracteres VARCHAR2(100) := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_+=';
      v_contador number:=0;
-	begin
-
-	while v_num_caracteres>v_contador  loop
+	BEGIN
+	WHILE v_num_caracteres>v_contador  LOOP
         v_contrasenya := v_contrasenya || substr(v_caracteres, dbms_random.value(1, length(v_caracteres)),1);
         v_contador := v_contador+1;
-    end loop;   
-	return v_contrasenya;
-end generar_contrasenya;
+    END LOOP;   
+	RETURN v_contrasenya;
+END generar_contrasenya;
 
-declare 
-    contrasena varchar2(100) := generar_contrasenya(100);
-begin
-    DBMS_OUTPUT.PUT_LINE(contrasena);
-end;
+-- creo la tabla que almacena la contraseña y el número del nuevo vendedor.
+CREATE TABLE vendedor_contrasenya  (
+            numvend  number(22) PRIMARY KEY,
+            contrasenya varchar2(100)  NOT NULL
+);
 
---creacion del trigger
-create or replace trigger relacion_contrasenya_vendedor after insert on vendedor for each row
-    declare
+--creo el trigger que se ejecutará despued de la inserción de un nuevo vendedor.
+CREATE OR REPLACE TRIGGER relacion_contrasenya_vendedor AFTER INSERT ON vendedor FOR EACH ROW
+    DECLARE
     	v_numvend vendedor.numvend%type;
 		v_contrasena varchar2(100);
-    begin 
+    BEGIN 
     	v_numvend := :new.numvend; 
-      	v_contrasena := generar_contrasenya(3); 
-	 	DBMS_OUTPUT.PUT_LINE('Creado vendedor con numero '||v_numvend|| ' y contraseña : ['||v_contrasena||']');
-end;
+      	v_contrasena := generar_contrasenya(6); 
+		INSERT INTO vendedor_contrasenya values(v_numvend,v_contrasena);          
+	 	DBMS_OUTPUT.PUT_LINE('Creada contraseña para el nuevo vendedor.');
+		DBMS_OUTPUT.PUT_LINE('Número de vendedor: [ '||v_numvend|| ' ]');
+		DBMS_OUTPUT.PUT_LINE('Contraseña : [ '||v_contrasena||' ]');
+    EXCEPTION 
+        WHEN DUP_VAL_ON_INDEX THEN  --inserta el vendedor, pero no falla como ya tiene una contraseña asignada
+        DBMS_OUTPUT.PUT_LINE('Error: El número de vendedor ya tiene una contraseña asignada.'); 
+		 WHEN OTHERS THEN
+    	DBMS_OUTPUT.PUT_LINE('Error: Se produjo un error al insertar en la tabla vendedor_contrasenya.');
+END;
+
 --pruebas
-INSERT INTO vendedor VALUES(33,'nombre de prueba','empresa de prueba','999333444','Avda. Valencia 3205','ALICANTE','ALICANTE');
-INSERT INTO vendedor VALUES(35,'nombre de prueba','empresa de prueba','999333444','Avda. Valencia 3205','ALICANTE','ALICANTE');
-
+delete from vendedor where NUMVEND=33;
+delete from vendedor_contrasenya where NUMVEND=33;
 select * from vendedor;
-
-
-
-
+select * from vendedor_contrasenya;
+select v.numvend, v.NOMVEND, vc.CONTRASENYA from vendedor v
+join vendedor_contrasenya vc on v.numvend=vc.numvend;
